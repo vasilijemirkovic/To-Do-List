acff32c4f43073b7f05680f3463d69d649b1fd55
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-01-05 12:40:22 +0100

    Repository for storing the IDs is created

---
+++ src/main/java/repository/IdRepository.java
@@ -0,0 +1,18 @@
+package repository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IdRepository {
+
+	private final Set<String> ids = new HashSet<>();
+
+	public synchronized boolean exists(String id) {
+		return ids.contains(id);
+	}
+
+	public synchronized void save(String id) {
+		ids.add(id);
+	}
+
+}
