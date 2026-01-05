da4c96e577236162517dc981fdc4690dd18c6632
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-01-05 12:41:13 +0100

    Presentation of the tasks improved.

---
+++ src/main/java/presentation/App.java
@@ -0,0 +1,57 @@
+package presentation;
+
+import java.util.Scanner;
+
+import controller.ListController;
+import model.Task;
+import repository.ToDoList;
+
+public class App {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		ToDoList todo = new ToDoList();
+		ListController controller = new ListController();
+		int choice;
+
+		do {
+			System.out.println("\n--- To-Do Lista ---");
+			System.out.println("1. Dodaj zadatak");
+			System.out.println("2. Prikazi zadatke");
+			System.out.println("3. Označi zadatak kao završen");
+			System.out.println("4. Obriši zadatak");
+			System.out.println("0. Izlaz");
+			System.out.print("Izaberite opciju: ");
+			choice = sc.nextInt();
+			sc.nextLine();
+
+			switch (choice) {
+			case 1:
+				System.out.print("Unesite opis zadatka: ");
+				String desc = sc.nextLine();
+				Task newTask = new Task(desc);
+				controller.addTaskToList(newTask, todo);
+				break;
+			case 2:
+				controller.printTasks(todo);
+				break;
+			case 3:
+				System.out.print("Unesite id zadatka koji je uspjesno zavrsen: ");
+				String doneIndex = sc.nextLine();
+				controller.completeTaskInListOfTasks(doneIndex, todo);
+				break;
+			case 4:
+				System.out.print("Unesite id zadatka za brisanje: ");
+				String id = sc.nextLine();
+				controller.removeTaskFromTheList(id, todo);
+				break;
+			case 0:
+				System.out.println("Izlaz iz programa...");
+				break;
+			default:
+				System.out.println("Nepoznata opcija!");
+			}
+		} while (choice != 0);
+		sc.close();
+	}
+}
